259a8e1496482c029806b5cf26839a8bc778b51b
Tiago Rocha <al2024146139@alunos.utad.pt>
2026-03-14 20:59:42 +0000

    Adiciona model Order

---
+++ WpfProjectLpdsw/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfProjectLpdsw.Models
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public string? OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalValue { get; set; }
+    }
+}
