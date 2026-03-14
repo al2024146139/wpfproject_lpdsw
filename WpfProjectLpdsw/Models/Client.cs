1df2a89ad95273fe2944913ec5eebf3ed68c9c2d
Tiago Rocha <al2024146139@alunos.utad.pt>
2026-03-14 17:41:29 +0000

    Adicionadas propriedades ao model do cliente

---
+++ WpfProjectLpdsw/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WpfProjectLpdsw.Models
+{
+    internal class Client
+    {
+        public int Id { get; set; }
+        public string ClientName { get; set; }
+        public string ClientEmail { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+
+    }
+}
