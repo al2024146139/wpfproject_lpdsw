a32c25a1bbb40331504673ee71d151e4960a2888
Tiago Rocha <al2024146139@alunos.utad.pt>
2026-03-14 21:03:07 +0000

    Adiciona model WarehouseStock

---
+++ WpfProjectLpdsw/Models/WarehouseStock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfProjectLpdsw.Models
+{
+    internal class WarehouseStock
+    {
+        public int Id { get; set; }
+        public string? ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string? Location { get; set; }
+    }
+}
